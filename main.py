8c892a6dbb977eada069e11a559a47ad6386f099
IglesiasT <iglesiastomas99@gmail.com>
2024-01-25 09:32:44 -0300

    add test detectar hojas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import fitz # PyMuPDF
+import fitz  # PyMuPDF
+
 
 def obtener_contenido_pdf(path_pdf) -> str:
     archivo_pdf = fitz.open(path_pdf)
@@ -8,30 +9,52 @@ def obtener_contenido_pdf(path_pdf) -> str:
         pagina = archivo_pdf.load_page(numero_pagina)
         texto += pagina.get_text()
     archivo_pdf.close()
-    
+
     return texto
+
 
 def son_iguales(file1, file2) -> bool:
     # Usar alguna estructura de datos para compararlos todos juntos?
     return obtener_contenido_pdf(file1) == obtener_contenido_pdf(file2)
 
-def obtener_pagina_donde_hay_diferencia(file1, file2):
-    # Retornar una sola y que la iteracion se haga afuera o que retorne una lista?
-    pass
+
+def obtener_paginas_donde_hay_diferencias(file1, file2) -> list:
+
+    pdf1 = fitz.open(file1)
+    pdf2 = fitz.open(file2)
+    paginas_diferentes = []
+
+    if pdf1.page_count != pdf2.page_count:
+        print('no tienen la misma cantidad de paginas')  # Manejar esto, no deberia romperse
+
+    for numero_pagina in range(pdf1.page_count):  # Pensar como evitar esta nueva iteracion
+        pagina_pdf1 = pdf1.load_page(numero_pagina)
+        pagina_pdf2 = pdf2.load_page(numero_pagina)
+
+        if pagina_pdf1.get_text() != pagina_pdf2.get_text():
+            paginas_diferentes.append(numero_pagina)
+
+    pdf1.close()
+    pdf2.close()
+
+    return paginas_diferentes
+
 
 def obtener_diferencias(file1, file2):
     # TODO: refactorizar para que reciba una lista de paths
-    
-    if not son_iguales(file1, file2):
-        print(obtener_pagina_donde_hay_diferencia(file1, file2))
-    else:
-        print('No hay diferencias')
+
+    if son_iguales(file1, file2):
+        return 'No hay diferencias'
+
+    return obtener_paginas_donde_hay_diferencias(file1, file2)
+
 
 def main():
     file1 = 'liviano.PDF'
     file2 = 'pesado.PDF'
 
     print(obtener_diferencias(file1, file2))
+
 
 if __name__ == '__main__':
     main()
