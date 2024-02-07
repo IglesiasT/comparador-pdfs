707868a3189ff070abc2a0822c1f3f0c1e5d26be
Tomás <iglesiastomas99@gmail.com>
2024-02-07 11:00:54 -0300

    add test extraer columnas productos

--- paginas/resumen.py
+++ paginas/resumen.py
@@ -8,7 +8,40 @@ class Resumen:
         self.diferencias = []
 
     def _extraer_indices(self) -> list:
+        texto_relevante = [
+            "PRODUCTOS DE LANZAMIENTO",
+            "PRIMERA LINEA DE PROMOCION",
+            "SEGUNDA LINEA DE PROMOCION",
+            "RECORDATORIOS",
+            "ENTRY MARKET"
+        ]
+
+        pagina_indice = self.paginas[0]
+        lineas = [linea.strip() for linea in pagina_indice.get_text().split('\n') if linea.strip()]
+
+        return [texto for texto in lineas if texto in texto_relevante]
+
+    def _extraer_columnas_productos(self) -> dict:
+        columnas_productos = ["Linea", "Marca", "Mercado", "MAT", "TRIM"]
+        # {
+        #   1: ["Linea", "Marca", "Mercado", "MAT", "TRIM"],
+        #   2: ["Linea", "Marca"],
+        #   3: ["Linea", "Marca", "Mercado", "MAT", "TRIM"]
+        # }
+        columnas_productos_extraidas = {}
+
+        paginas_mercados = self.paginas[1:]
+
+        for pagina in paginas_mercados:
+            lineas = [linea.strip() for linea in pagina.get_text().split('\n') if linea.strip()]
+            columnas_productos_extraidas[pagina.number] = [texto for texto in lineas if texto in columnas_productos]
+
+        return columnas_productos_extraidas
+
+    def obtener_diferencias(self, otro_resumen):
         """
+        ej de output esperado para luego comparar
+        la idea es obtener primero el contenido y que luego por fuera se compare (renombrar funcion)
         {"indices": [
             PRODUCTOS DE LANZAMIENTO
             PRIMERA LINEA DE PROMOCION
@@ -19,53 +52,14 @@ class Resumen:
         "columnas_mercados": [Linea, Marca, Mercado, MAT, TRIM]
         }
         """
-        texto_relevante = [
-            "PRODUCTOS DE LANZAMIENTO",
-            "PRIMERA LINEA DE PROMOCION",
-            "SEGUNDA LINEA DE PROMOCION",
-            "RECORDATORIOS",
-            "ENTRY MARKET"
-            ]
-
-        pagina_indice = self.paginas[0]
-        lineas = [linea.strip() for linea in pagina_indice.get_text().split('\n') if linea.strip()]
-
-        return [texto for texto in lineas if texto in texto_relevante]
-
-    def _comparar_cantidad_paginas(self, cantidad_paginas_otro_resumen: int):
-        """
-        Si esto es un template al cual se compara el output de los resumenes
-        generados, tiene sentido que haya una cantidad de paginas establecida?
-        """
-        pass
-        # if len(self.paginas) != cantidad_paginas_otro_resumen:
-        #     self.diferencias.append(DistintaCantidadDePaginas(
-        #     ))
-
-    def _comparar_contenido_texto(self, pagina1, pagina2):
-        """
-        En algun lado debe decir Resumen
-        Primer pagina resumen:
-            PRODUCTOS DE LANZAMIENTO
-            PRIMERA LINEA DE PROMOCION
-            SEGUNDA LINEA DE PROMOCION
-            RECORDATORIOS
-            ENTRY MARKET
-        """
-        assert pagina1.number == pagina2.number
-
-        if pagina1.get_text() != pagina2.get_text():
-            self.diferencias.append(DistintoTexto(pagina1.number))
-
-    def obtener_diferencias(self, otro_resumen):
         # TODO refactor
 
-        self._comparar_cantidad_paginas(len(otro_resumen.paginas))
-        #self._comparar_pagina_indices(otro_resumen.paginas[0])
+        # self._comparar_cantidad_paginas(len(otro_resumen.paginas))
+        # self._comparar_pagina_indices(otro_resumen.paginas[0])
         # Notar que la ejecucion sigue aunque haya distinta cantidad de paginas
-        for pagina1, pagina2 in zip(self.paginas, otro_resumen.paginas):
-            # self._comparar_paginas(pagina1, pagina2)
-            self._comparar_contenido_texto(pagina1, pagina2)
+        # for pagina1, pagina2 in zip(self.paginas, otro_resumen.paginas):pass
+        # self._comparar_paginas(pagina1, pagina2)
+        # self._comparar_contenido_texto(pagina1, pagina2)
 
         for diferencia in self.diferencias:
             diferencia.mostrar()
--- tests_resumen.py
+++ tests_resumen.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 import fitz
 
@@ -15,6 +14,11 @@ class TestResumen(unittest.TestCase):
         paginas_resumen = [pag for pag in self.pdf_prueba.pages(stop=5)]
         self.resumen = Resumen(paginas_resumen)
 
+    def tearDown(self):
+        # Cerramos el archivo luego de cada test independientemente de si falla o no
+        if hasattr(self, 'pdf_prueba') and hasattr(self.pdf_prueba, 'close'):
+            self.pdf_prueba.close()
+
     def test_extraer_indice_devuelve_indices_de_la_primer_pagina(self):
         indices_esperados = [
             "PRODUCTOS DE LANZAMIENTO",
@@ -25,7 +29,11 @@ class TestResumen(unittest.TestCase):
             ]
 
         self.assertEqual(self.resumen._extraer_indices(), indices_esperados)
-        self.pdf_prueba.close()
+
+    def test_extraer_columnas_productos_devuelve_headers_tablas(self):
+        columnas_esperadas = ["Linea", "Marca", "Mercado", "MAT", "TRIM"]
+
+        self.assertEqual(self.resumen._extraer_columnas_productos()[1], columnas_esperadas)
 
 
 if __name__ == '__main__':
