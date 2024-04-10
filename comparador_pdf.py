5bdf46261736094bbbdbeef189f8fb7f76ddf11c
Tomás <iglesiastomas99@gmail.com>
2024-04-10 17:28:35 -0300

    delete obtener_diferencias

--- comparador_pdf.py
+++ comparador_pdf.py
@@ -3,6 +3,7 @@ import fitz  # PyMuPDF
 
 from diferencias.distinta_cantidad_de_paginas import DistintaCantidadDePaginas
 from diferencias.distinto_texto import DistintoTexto
+from tipos_hojas.resumen import Resumen
 
 
 class ComparadorPDF:
@@ -26,25 +27,6 @@ class ComparadorPDF:
 
         return archivos_con_pares
 
-    def obtener_diferencias(self, pdf1: fitz.Document, pdf2: fitz.Document) -> list:
-        """
-        Se toma como supuesto que los archivos ya vienen abiertos
-        """
-
-        paginas_con_diferencias = []
-
-        if pdf1.page_count != pdf2.page_count:
-            return [DistintaCantidadDePaginas(pdf1.page_count, pdf2.page_count)]
-
-        for numero_pagina in range(pdf1.page_count):    # Sabiendo que ambos tienen misma cantidad de páginas
-            pagina_pdf1 = pdf1.load_page(numero_pagina)
-            pagina_pdf2 = pdf2.load_page(numero_pagina)
-
-            if pagina_pdf1.get_text() != pagina_pdf2.get_text():
-                paginas_con_diferencias.append(DistintoTexto(numero_pagina + 1))
-
-        return paginas_con_diferencias
-
     def obtener_archivos_con_diferencias(self) -> dict:
         """
         Devuelve un diccionario con los nombres de los archivos que tienen diferencias
@@ -57,8 +39,25 @@ class ComparadorPDF:
         for archivo in archivos_a_comparar:
             pdf1 = fitz.open(os.path.join(self._input1, archivo))
             pdf2 = fitz.open(os.path.join(self._input2, archivo))
+            diferencias = []
 
-            archivos_con_diferencias[archivo] = self.obtener_diferencias(pdf1, pdf2)
+            if pdf1.page_count != pdf2.page_count:
+                archivos_con_diferencias[archivo] = [DistintaCantidadDePaginas(pdf1.page_count, pdf2.page_count)]
+                continue
+
+            for numero_pagina in range(pdf1.page_count):    # Sabiendo que ambos tienen misma cantidad de páginas
+                pagina_pdf1 = pdf1.load_page(numero_pagina)
+                pagina_pdf2 = pdf2.load_page(numero_pagina)
+
+                if pagina_pdf1.get_text() != pagina_pdf2.get_text():
+                    diferencias.append(DistintoTexto(numero_pagina + 1))
+
+            if diferencias:
+                archivos_con_diferencias[archivo] = diferencias # Si hay diferencias, se agrega al diccionario
+            
+            # El comparador es quien conoce entre que rangos hay cada tipo de pagina
+            resumen1 = Resumen(pdf1.pages(0,6))
+            resumen2 = Resumen(pdf2.pages(0,6))
 
             pdf1.close()
             pdf2.close()
